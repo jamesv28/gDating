11bb8cc14a1475719526c3f3127de0a4ddc0a38e
James Volmert <james.volmert@gmail.com>
2016-04-19 19:11:20 -0600

    fixed some styling

--- src/js/controllers/membersCtrl.js
+++ src/js/controllers/membersCtrl.js
@@ -1,3 +1,11 @@
 app.controller('membersCtrl', ['$scope', 'getMembersService', function($scope, getMembersService) {
 
+    getMembersService.getallMembers()
+        .then(function(data){
+            $scope.members = data.data.data;
+            console.log('members', $scope.members);
+        });
+
+
+
 }]);
--- src/js/services/membersSerivces.js
+++ src/js/services/membersSerivces.js
@@ -1,10 +1,16 @@
 app.service('getMembersService',['$http', function($http){
-    return $http({
-        method: 'GET',
-        url: 'https://galvanize-student-apis.herokuapp.com/gdating/members/'
-    })
-        .then(function (results) {
-            console.log(results);
-        });
+    return {
+        getallMembers: function () {
+            return $http({
+                method: 'GET',
+                url: 'https://galvanize-student-apis.herokuapp.com/gdating/members/'
+            })
+                .then(function (results) {
+                    console.log('some members',results);
+                    return results;
+                });
+        }
+
+        }; // end of return statement
 
 }]); //end of service
